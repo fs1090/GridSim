0e10a0839608f43d66812a33e259b61d0deae7a1
fs1090 <your_email@example.com>
2025-08-29 14:41:13 -0400

    .gitignore

--- app/src/main/java/com/example/gridsim/MainActivity.java
+++ app/src/main/java/com/example/gridsim/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gridsim;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // connect to XML layout
     }
+
+    Button button1 = findViewById(R.id.button1);
+    Button button2 = findViewById(R.id.button2);
+
+
 }

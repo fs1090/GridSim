c5c76ccc6a857a65a7d99c2fcedf15f9361bb4ab
fs1090 <your_email@example.com>
2025-08-29 14:28:37 -0400

    gitignore update

--- app/src/main/java/com/example/gridsim/MainActivity.java
+++ app/src/main/java/com/example/gridsim/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gridsim;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
